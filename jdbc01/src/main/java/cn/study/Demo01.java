532dadabee2ead294bfd6001701adffcebf55634
DESKTOP-RINLN9T\wm <juststrive007@gmail.com>
2021-03-21 22:31:47 +0800

    Study:2021/3/21 JDBC 开始学习

---
+++ jdbc01/src/main/java/cn/study/Demo01.java
@@ -0,0 +1,13 @@
+package cn.study;
+
+public class Demo01 {
+    public static void main(String[] args) {
+        //1.注册驱动
+//        Class.forName("");
+        //2.获取连接对象
+
+        //3.创建SQL语句执行对象
+        //4.执行SQL
+        //5.关闭资源
+    }
+}
