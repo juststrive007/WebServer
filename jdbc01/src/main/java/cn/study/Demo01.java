ba859acfcb0256c9ace289255378c2a20bdc5377
DESKTOP-RINLN9T\wm <juststrive007@gmail.com>
2021-03-28 22:51:12 +0800

    Study：2021-3-28 JDBC connection

--- jdbc01/src/main/java/cn/study/Demo01.java
+++ jdbc01/src/main/java/cn/study/Demo01.java
@@ -1,11 +1,21 @@
 package cn.study;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Demo01 {
-    public static void main(String[] args) {
-        //1.注册驱动
-//        Class.forName("");
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        //1.注册驱动 运行提示ClassNotFound 把这个类
+//        Class.forName("com.mysql.jdbc.Driver");
+
         //2.获取连接对象
 
+            Connection conn= DriverManager.getConnection(
+                    "jdbc:mysql://192.168.0.8:3306/newdb3",
+                    "root","123456");
+
+        System.out.println(conn);
         //3.创建SQL语句执行对象
         //4.执行SQL
         //5.关闭资源
